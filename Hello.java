38e2245bbe061f66a7363807c9dd73a6937a4010
Tyler Middendorf <34323666+Tyler-Middendorf@users.noreply.github.com>
2019-11-17 14:52:27 -0600

    Formatting
    uwu

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public helloJava{
   public static void helloJava(){
-  System.out.println("Hello From The Bestest Language Ever, JAVA!!!");
+    System.out.println("Hello From The Bestest Language Ever, JAVA!!!");
   }
 }
