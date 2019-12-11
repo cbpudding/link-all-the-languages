8b5ce95cf9dc8f8002437266594db24565f2f820
Alexander Hill <CodeBreadpudding@gmail.com>
2019-12-11 15:19:32 -0500

    Fixed Zig(Thanks GrayJack!)

--- src/lib.zig
+++ src/lib.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 export fn hello_zig() void {
     const stdout = &std.io.getStdOut().outStream().stream;
-    if (stdout.print("Hello from Zig!\n")) |_| {} else |err| {
-        std.debug.warn("{}", err);
+    if (stdout.print("Hello from Zig!\n", .{})) |_| {} else |err| {
+        std.debug.warn("{}", .{err});
     }
 }
