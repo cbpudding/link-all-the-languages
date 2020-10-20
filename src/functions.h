a81dc2cf4510e78e9e26774b53bc8648af7ccb61
Monadic Cat <greenmonadcat@gmail.com>
2020-10-20 11:45:55 -0500

    Remove Zig, since it no longer builds successfully.

--- src/functions.h
+++ src/functions.h
@@ -5,7 +5,6 @@ extern const void hello_d();
 extern const void hello_fortran();
 extern const void hello_nim();
 extern const void hello_rust();
-extern const void hello_zig();
 
-const void (*hello[])() = { hello_c, hello_cpp, hello_carp, hello_d, hello_fortran, hello_nim, hello_rust, hello_zig };
+const void (*hello[])() = { hello_c, hello_cpp, hello_carp, hello_d, hello_fortran, hello_nim, hello_rust };
 #define NUMBER_OF_LANGUAGES sizeof(hello) / sizeof(hello[0])
