e770611b870db16432e41e564f948fe08452593d
Alexander Hill <CodeBreadpudding@gmail.com>
2021-04-02 17:56:07 -0400

    Fixed the headers for Lua 5.3

--- src/lua/wrapper.c
+++ src/lua/wrapper.c
@@ -1,6 +1,6 @@
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
+#include <lua5.3/lua.h>
+#include <lua5.3/lauxlib.h>
+#include <lua5.3/lualib.h>
 
 void hello_lua() {
     lua_State *L = luaL_newstate();
