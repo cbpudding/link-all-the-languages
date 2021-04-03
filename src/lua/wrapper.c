6a7af1f44be446a5cf056afa08a780a94d7435c8
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2021-04-03 11:05:01 -0400

    First attempt at fixing the lua implementation

--- src/lua/wrapper.c
+++ src/lua/wrapper.c
@@ -2,9 +2,14 @@
 #include <lua5.3/lauxlib.h>
 #include <lua5.3/lualib.h>
 
-void hello_lua() {
+#include "luascript.h"
+
+void call_hello_lua() {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
-    // ...
+    
+    luaL_loadstring(L, hello_lua);
+    lua_pcall(L, 0, LUA_MULTRET, 0);
+
     lua_close(L);
 }
