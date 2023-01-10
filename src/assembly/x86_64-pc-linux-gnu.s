b986a6f431b6336294e865842e812356543a7579
Alexander Hill <ahill@breadpudding.dev>
2023-01-09 19:47:28 -0500

    Completed the challenge in x86 Assembly!

--- src/assembly/x86_64-pc-linux-gnu.s
+++ src/assembly/x86_64-pc-linux-gnu.s
@@ -2,15 +2,25 @@
 hello: .ascii "Hello from Assembly!\n"
 hello_len = . - hello
     .text
+    .globl challenge
     .globl hello_assembly
     .type hello_assembly, @function
 hello_assembly:
+    /* Create a new call frame */
     push %rbp
     mov %rsp, %rbp
+    /* Complete the challenge */
+    mov %edi, %eax
+    add $5, %eax
+    mov %eax, %edi
+    call challenge
+    /* Print our message from Assembly */
     mov $1, %rax
     mov $1, %rdi
     lea hello(%rip), %rsi
     mov $hello_len, %rdx
     syscall
+    /* Restore the old call frame and return */
+    mov %rbp, %rsp
     pop %rbp
     ret
