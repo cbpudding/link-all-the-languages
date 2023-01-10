4d9c7a5a4b08b26d17dd0c93cd352d94bbbdbe21
Alexander Hill <ahill@breadpudding.dev>
2023-01-09 19:50:15 -0500

    Completed C++

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+extern "C" {
+    #include "challenge.h"
+}
+
 using namespace std;
 
-extern "C" void hello_cpp() {
+extern "C" void hello_cpp(int c) {
     cout << "Hello from C++!" << endl;
+    challenge(c + 5);
 }
